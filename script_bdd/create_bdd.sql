5b3651679cb85c0093cf24d9e7e7ec27d8261e78
Swann HERERERA <swann.herrera@gmail.com>
2020-03-15 11:30:03 +0100

    start

---
+++ script_bdd/create_bdd.sql
@@ -0,0 +1,294 @@
+CREATE TYPE location AS (
+    longitude DOUBLE PRECISION,
+    latitude DOUBLE PRECISION
+);
+
+CREATE TYPE schedule AS (
+    text_schedule TEXT
+);
+
+CREATE TYPE customer_status AS ENUM (
+    'banned',
+    'active',
+    'unvalidated'
+);
+
+CREATE TYPE type_address AS ENUM (
+    'facturation',
+    'livraison'
+);
+
+CREATE TYPE type_promo AS ENUM (
+    'all',
+    'code',
+    'product'
+);
+
+CREATE TYPE ticket_status AS ENUM (
+    'open',
+    'close',
+    'in progress'
+);
+
+
+CREATE TYPE type_event AS ENUM (
+    'tasting',
+    'paulée',
+    'wine&music',
+    'masterclass'
+);
+
+-- table
+
+CREATE table franchisee (
+    id SERIAL UNIQUE NOT NULL PRIMARY KEY,
+    name VARCHAR[100],
+    qr_code VARCHAR[255],
+    created_at TIMESTAMP
+);
+
+CREATE TABLE admin_franchisee (
+    id serial UNIQUE NOT NULL PRIMARY KEY,
+    first_name VARCHAR[100],
+    last_name VARCHAR[100],
+    email VARCHAR[255],
+    password CHAR[60],
+    created_at TIMESTAMP
+);
+
+CREATE TABLE access_franchisee (
+    id SERIAL UNIQUE NOT NULL PRIMARY KEY,
+    name VARCHAR[120],
+    created_at TIMESTAMP
+);
+
+CREATE TABLE access_franchisee_x_admin_franchisee (
+    admin_franchisee_id INTEGER REFERENCES admin_franchisee(id),
+    access_franchisee_id INTEGER REFERENCES access_franchisee(id),
+    PRIMARY KEY (admin_franchisee_id, access_franchisee_id)
+);
+
+CREATE TABLE franchisee_x_admin_franchisee (
+    franchisee_id INTEGER REFERENCES franchisee(id),
+    admin_franchisee_id INTEGER REFERENCES admin_franchisee(id),
+    PRIMARY KEY (franchisee_id, admin_franchisee_id)
+);
+
+CREATE TABLE truck (
+    id SERIAL UNIQUE NOT NULL PRIMARY KEY,
+    franchisees_id INTEGER REFERENCES franchisee(id),
+    name VARCHAR[100],
+    location location,
+    created_at TIMESTAMP,
+    schedule schedule
+);
+
+CREATE TABLE promotion (
+    id SERIAL UNIQUE NOT NULL PRIMARY KEY,
+    name VARCHAR[120],
+    type type_promo,
+    amount REAL,
+    start_at TIMESTAMP,
+    end_at TIMESTAMP,
+    created_at TIMESTAMP
+);
+
+CREATE TABLE promotion_x_truck (
+    promotion_id INTEGER REFERENCES promotion(id),
+    truck_id INTEGER REFERENCES truck(id),
+    PRIMARY KEY (promotion_id, truck_id)
+);
+
+CREATE TABLE customer (
+    id SERIAL UNIQUE NOT NULL PRIMARY KEY,
+    first_name VARCHAR[100],
+    last_name VARCHAR[100],
+    email VARCHAR[255],
+    password CHAR[60],
+    created_at TIMESTAMP,
+    status customer_status
+);
+
+CREATE TABLE ticket (
+    id SERIAL UNIQUE NOT NULL PRIMARY KEY,
+    customer_id INTEGER REFERENCES customer(id),
+    text text,
+    created_at TIMESTAMP,
+    status ticket_status DEFAULT 'open'
+);
+
+CREATE TABLE fidelity_card (
+    id SERIAL UNIQUE NOT NULL PRIMARY KEY,
+    customer_id INTEGER REFERENCES customer(id),
+    perumtion_date TIMESTAMP,
+    created_at TIMESTAMP,
+    point INTEGER
+);
+
+CREATE TABLE address (
+    id SERIAL UNIQUE NOT NULL PRIMARY KEY,
+    id_customer INTEGER REFERENCES customer(id),
+    country VARCHAR[255],
+    city VARCHAR[255],
+    number VARCHAR[30],
+    address_line VARCHAR[255],
+    type type_address,
+    created_at TIMESTAMP
+);
+
+CREATE TABLE event (
+    id SERIAL UNIQUE NOT NULL PRIMARY KEY,
+    name VARCHAR[120],
+    start_at TIMESTAMP,
+    end_at TIMESTAMP,
+    created_at TIMESTAMP,
+    enter_price REAL,
+    type type_event
+);
+
+CREATE TABLE event_x_truck (
+    event_id INTEGER REFERENCES event(id),
+    truck_id INTEGER REFERENCES truck(id),
+    PRIMARY KEY (event_id, truck_id)
+);
+
+CREATE TABLE event_x_customer_id (
+    event_id INTEGER REFERENCES event(id),
+    customer_id INTEGER REFERENCES customer(id),
+    PRIMARY KEY (event_id, customer_id)
+);
+
+CREATE TABLE admin (
+    id SERIAL UNIQUE NOT NULL PRIMARY KEY,
+    first_name VARCHAR[100],
+    last_name VARCHAR[100],
+    email VARCHAR[255],
+    password CHAR[60],
+    created_at TIMESTAMP
+);
+
+CREATE TABLE access_admin (
+    id SERIAL UNIQUE NOT NULL PRIMARY KEY,
+    name VARCHAR[120],
+    created_at TIMESTAMP
+);
+
+CREATE TABLE access_admin_x_admin (
+    admin_id INTEGER REFERENCES admin(id),
+    access_admin_id INTEGER REFERENCES access_admin(id),
+    PRIMARY KEY (admin_id, access_admin_id)
+);
+
+CREATE TABLE provider (
+    id SERIAL UNIQUE NOT NULL PRIMARY KEY,
+    name VARCHAR[120],
+    contact VARCHAR[255],
+    country VARCHAR[255],
+    created_at TIMESTAMP
+);
+
+CREATE TABLE categories (
+    id SERIAL UNIQUE NOT NULL PRIMARY KEY,
+    name VARCHAR[120],
+    created_at TIMESTAMP
+);
+
+CREATE TABLE item (
+    id SERIAL UNIQUE NOT NULL PRIMARY KEY,
+    name VARCHAR[255],
+    price REAL,
+    categories INTEGER REFERENCES categories(id),
+    created_at TIMESTAMP
+);
+
+CREATE TABLE warehouse (
+    id SERIAL UNIQUE NOT NULL PRIMARY KEY,
+    location location,
+    name VARCHAR[100],
+    created_at TIMESTAMP
+);
+
+CREATE TABLE item_stock (
+    warehouses_id  INTEGER REFERENCES warehouse(id),
+    item_id INTEGER REFERENCES item(id),
+    quantity INTEGER,
+    PRIMARY KEY (warehouses_id, item_id)
+);
+
+CREATE TABLE order_provider (
+    id SERIAL UNIQUE NOT NULL PRIMARY KEY,
+    provider_id INTEGER REFERENCES provider(id),
+    admin_id INTEGER REFERENCES admin(id),
+    created_at TIMESTAMP
+);
+
+CREATE TABLE order_provider_line (
+    order_provider_id INTEGER REFERENCES order_provider(id),
+    item_id INTEGER,
+    quantity REAL,
+    PRIMARY KEY (order_provider_id, item_id)
+);
+
+CREATE TABLE order_franchisee (
+    id SERIAL UNIQUE NOT NULL PRIMARY KEY,
+    admin_Franchisees_id INTEGER REFERENCES admin_franchisee(id),
+    created_at TIMESTAMP
+);
+
+CREATE TABLE order_franchisee_line (
+    order_franchisee_id INTEGER REFERENCES order_franchisee(id),
+    item_id INTEGER REFERENCES item(id),
+    quantity INTEGER,
+    price REAL,
+    PRIMARY KEY (order_franchisee_id, item_id)
+);
+
+CREATE TABLE customer_order (
+    id SERIAL UNIQUE NOT NULL PRIMARY KEY,
+    customer_id INTEGER REFERENCES customer(id),
+    created_at TIMESTAMP,
+    trucks_id INTEGER REFERENCES truck(id)
+);
+
+CREATE TABLE card_categories (
+    id  SERIAL UNIQUE NOT NULL PRIMARY KEY,
+    name VARCHAR[100],
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP
+);
+
+CREATE TABLE card (
+    id  SERIAL UNIQUE NOT NULL PRIMARY KEY,
+    name varchar[120],
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    truck_id INTEGER REFERENCES truck(id)
+);
+
+CREATE TABLE card_categories_x_card (
+    card_id  INTEGER REFERENCES card(id),
+    card_categorie_id INTEGER REFERENCES card_categories(id),
+    PRIMARY KEY (card_id, card_categorie_id)
+);
+
+CREATE TABLE card_item (
+    id SERIAL UNIQUE NOT NULL PRIMARY KEY,
+    card_categories_id INTEGER REFERENCES card_categories(id),
+    name VARCHAR[120],
+    price REAL,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP
+);
+
+CREATE TABLE promotion_x_card_item (
+    promotion_id  INTEGER REFERENCES promotion(id),
+    card_item_id INTEGER REFERENCES card_item(id),
+    PRIMARY KEY (promotion_id, card_item_id)
+);
+
+CREATE TABLE customer_order_line (
+    customer_order_id INTEGER REFERENCES customer_order(id),
+    card_item_id INTEGER REFERENCES card_item(id),
+    quantity REAL,
+    PRIMARY KEY (customer_order_id, card_item_id)
+);
---
+++ script_bdd/datagrip_create.sql
@@ -0,0 +1,551 @@
+create type location as
+(
+    longitude double precision,
+    latitude  double precision
+);
+
+alter type location owner to postgres;
+
+create type schedule as
+(
+    text_schedule text
+);
+
+alter type schedule owner to postgres;
+
+create type customer_status as enum ('banned', 'active', 'unvalidated');
+
+alter type customer_status owner to postgres;
+
+create type type_address as enum ('facturation', 'livraison');
+
+alter type type_address owner to postgres;
+
+create type type_promo as enum ('all', 'code', 'product');
+
+alter type type_promo owner to postgres;
+
+create type ticket_status as enum ('open', 'close', 'in progress');
+
+alter type ticket_status owner to postgres;
+
+create type type_event as enum ('tasting', 'paulée', 'wine&music', 'masterclass');
+
+alter type type_event owner to postgres;
+
+create table franchisee
+(
+    id         serial not null
+        constraint franchisee_pkey
+            primary key,
+    name       character varying[],
+    qr_code    character varying[],
+    created_at timestamp
+);
+
+alter table franchisee
+    owner to postgres;
+
+create table admin_franchisee
+(
+    id         serial not null
+        constraint admin_franchisee_pkey
+            primary key,
+    first_name character varying[],
+    last_name  character varying[],
+    email      character varying[],
+    password   char[],
+    created_at timestamp
+);
+
+alter table admin_franchisee
+    owner to postgres;
+
+create table access_franchisee
+(
+    id         serial not null
+        constraint access_franchisee_pkey
+            primary key,
+    name       character varying[],
+    created_at timestamp
+);
+
+alter table access_franchisee
+    owner to postgres;
+
+create table access_franchisee_x_admin_franchisee
+(
+    admin_franchisee_id  integer not null
+        constraint access_franchisee_x_admin_franchisee_admin_franchisee_id_fkey
+            references admin_franchisee,
+    access_franchisee_id integer not null
+        constraint access_franchisee_x_admin_franchisee_access_franchisee_id_fkey
+            references access_franchisee,
+    constraint access_franchisee_x_admin_franchisee_pkey
+        primary key (admin_franchisee_id, access_franchisee_id)
+);
+
+alter table access_franchisee_x_admin_franchisee
+    owner to postgres;
+
+create table franchisee_x_admin_franchisee
+(
+    franchisee_id       integer not null
+        constraint franchisee_x_admin_franchisee_franchisee_id_fkey
+            references franchisee,
+    admin_franchisee_id integer not null
+        constraint franchisee_x_admin_franchisee_admin_franchisee_id_fkey
+            references admin_franchisee,
+    constraint franchisee_x_admin_franchisee_pkey
+        primary key (franchisee_id, admin_franchisee_id)
+);
+
+alter table franchisee_x_admin_franchisee
+    owner to postgres;
+
+create table truck
+(
+    id             serial not null
+        constraint truck_pkey
+            primary key,
+    franchisees_id integer
+        constraint truck_franchisees_id_fkey
+            references franchisee,
+    name           character varying[],
+    location       location,
+    created_at     timestamp,
+    schedule       schedule
+);
+
+alter table truck
+    owner to postgres;
+
+create table promotion
+(
+    id         serial not null
+        constraint promotion_pkey
+            primary key,
+    name       character varying[],
+    type       type_promo,
+    amount     real,
+    start_at   timestamp,
+    end_at     timestamp,
+    created_at timestamp
+);
+
+alter table promotion
+    owner to postgres;
+
+create table promotion_x_truck
+(
+    promotion_id integer not null
+        constraint promotion_x_truck_promotion_id_fkey
+            references promotion,
+    truck_id     integer not null
+        constraint promotion_x_truck_truck_id_fkey
+            references truck,
+    constraint promotion_x_truck_pkey
+        primary key (promotion_id, truck_id)
+);
+
+alter table promotion_x_truck
+    owner to postgres;
+
+create table customer
+(
+    id         serial not null
+        constraint customer_pkey
+            primary key,
+    first_name character varying[],
+    last_name  character varying[],
+    email      character varying[],
+    password   char[],
+    created_at timestamp,
+    status     customer_status
+);
+
+alter table customer
+    owner to postgres;
+
+create table ticket
+(
+    id          serial not null
+        constraint ticket_pkey
+            primary key,
+    customer_id integer
+        constraint ticket_customer_id_fkey
+            references customer,
+    text        text,
+    created_at  timestamp,
+    status      ticket_status default 'open'::ticket_status
+);
+
+alter table ticket
+    owner to postgres;
+
+create table fidelity_card
+(
+    id             serial not null
+        constraint fidelity_card_pkey
+            primary key,
+    customer_id    integer
+        constraint fidelity_card_customer_id_fkey
+            references customer,
+    perumtion_date timestamp,
+    created_at     timestamp,
+    point          integer
+);
+
+alter table fidelity_card
+    owner to postgres;
+
+create table address
+(
+    id           serial not null
+        constraint address_pkey
+            primary key,
+    id_customer  integer
+        constraint address_id_customer_fkey
+            references customer,
+    country      character varying[],
+    city         character varying[],
+    number       character varying[],
+    address_line character varying[],
+    type         type_address,
+    created_at   timestamp
+);
+
+alter table address
+    owner to postgres;
+
+create table event
+(
+    id          serial not null
+        constraint event_pkey
+            primary key,
+    name        character varying[],
+    start_at    timestamp,
+    end_at      timestamp,
+    created_at  timestamp,
+    enter_price real,
+    type        type_event
+);
+
+alter table event
+    owner to postgres;
+
+create table event_x_truck
+(
+    event_id integer not null
+        constraint event_x_truck_event_id_fkey
+            references event,
+    truck_id integer not null
+        constraint event_x_truck_truck_id_fkey
+            references truck,
+    constraint event_x_truck_pkey
+        primary key (event_id, truck_id)
+);
+
+alter table event_x_truck
+    owner to postgres;
+
+create table event_x_customer_id
+(
+    event_id    integer not null
+        constraint event_x_customer_id_event_id_fkey
+            references event,
+    customer_id integer not null
+        constraint event_x_customer_id_customer_id_fkey
+            references customer,
+    constraint event_x_customer_id_pkey
+        primary key (event_id, customer_id)
+);
+
+alter table event_x_customer_id
+    owner to postgres;
+
+create table admin
+(
+    id         serial not null
+        constraint admin_pkey
+            primary key,
+    first_name character varying[],
+    last_name  character varying[],
+    email      character varying[],
+    password   char[],
+    created_at timestamp
+);
+
+alter table admin
+    owner to postgres;
+
+create table access_admin
+(
+    id         serial not null
+        constraint access_admin_pkey
+            primary key,
+    name       character varying[],
+    created_at timestamp
+);
+
+alter table access_admin
+    owner to postgres;
+
+create table access_admin_x_admin
+(
+    admin_id        integer not null
+        constraint access_admin_x_admin_admin_id_fkey
+            references admin,
+    access_admin_id integer not null
+        constraint access_admin_x_admin_access_admin_id_fkey
+            references access_admin,
+    constraint access_admin_x_admin_pkey
+        primary key (admin_id, access_admin_id)
+);
+
+alter table access_admin_x_admin
+    owner to postgres;
+
+create table provider
+(
+    id         serial not null
+        constraint provider_pkey
+            primary key,
+    name       character varying[],
+    contact    character varying[],
+    country    character varying[],
+    created_at timestamp
+);
+
+alter table provider
+    owner to postgres;
+
+create table categories
+(
+    id         serial not null
+        constraint categories_pkey
+            primary key,
+    name       character varying[],
+    created_at timestamp
+);
+
+alter table categories
+    owner to postgres;
+
+create table item
+(
+    id         serial not null
+        constraint item_pkey
+            primary key,
+    name       character varying[],
+    price      real,
+    categories integer
+        constraint item_categories_fkey
+            references categories,
+    created_at timestamp
+);
+
+alter table item
+    owner to postgres;
+
+create table warehouse
+(
+    id         serial not null
+        constraint warehouse_pkey
+            primary key,
+    location   location,
+    name       character varying[],
+    created_at timestamp
+);
+
+alter table warehouse
+    owner to postgres;
+
+create table item_stock
+(
+    warehouses_id integer not null
+        constraint item_stock_warehouses_id_fkey
+            references warehouse,
+    item_id       integer not null
+        constraint item_stock_item_id_fkey
+            references item,
+    quantity      integer,
+    constraint item_stock_pkey
+        primary key (warehouses_id, item_id)
+);
+
+alter table item_stock
+    owner to postgres;
+
+create table order_provider
+(
+    id          serial not null
+        constraint order_provider_pkey
+            primary key,
+    provider_id integer
+        constraint order_provider_provider_id_fkey
+            references provider,
+    admin_id    integer
+        constraint order_provider_admin_id_fkey
+            references admin,
+    created_at  timestamp
+);
+
+alter table order_provider
+    owner to postgres;
+
+create table order_provider_line
+(
+    order_provider_id integer not null
+        constraint order_provider_line_order_provider_id_fkey
+            references order_provider,
+    item_id           integer not null,
+    quantity          real,
+    constraint order_provider_line_pkey
+        primary key (order_provider_id, item_id)
+);
+
+alter table order_provider_line
+    owner to postgres;
+
+create table order_franchisee
+(
+    id                   serial not null
+        constraint order_franchisee_pkey
+            primary key,
+    admin_franchisees_id integer
+        constraint order_franchisee_admin_franchisees_id_fkey
+            references admin_franchisee,
+    created_at           timestamp
+);
+
+alter table order_franchisee
+    owner to postgres;
+
+create table order_franchisee_line
+(
+    order_franchisee_id integer not null
+        constraint order_franchisee_line_order_franchisee_id_fkey
+            references order_franchisee,
+    item_id             integer not null
+        constraint order_franchisee_line_item_id_fkey
+            references item,
+    quantity            integer,
+    price               real,
+    constraint order_franchisee_line_pkey
+        primary key (order_franchisee_id, item_id)
+);
+
+alter table order_franchisee_line
+    owner to postgres;
+
+create table customer_order
+(
+    id          serial not null
+        constraint customer_order_pkey
+            primary key,
+    customer_id integer
+        constraint customer_order_customer_id_fkey
+            references customer,
+    created_at  timestamp,
+    trucks_id   integer
+        constraint customer_order_trucks_id_fkey
+            references truck
+);
+
+alter table customer_order
+    owner to postgres;
+
+create table card_categories
+(
+    id         serial not null
+        constraint card_categories_pkey
+            primary key,
+    name       character varying[],
+    created_at timestamp,
+    updated_at timestamp
+);
+
+alter table card_categories
+    owner to postgres;
+
+create table card
+(
+    id         serial not null
+        constraint card_pkey
+            primary key,
+    name       character varying[],
+    created_at timestamp,
+    updated_at timestamp,
+    truck_id   integer
+        constraint card_truck_id_fkey
+            references truck
+);
+
+alter table card
+    owner to postgres;
+
+create table card_categories_x_card
+(
+    card_id           integer not null
+        constraint card_categories_x_card_card_id_fkey
+            references card,
+    card_categorie_id integer not null
+        constraint card_categories_x_card_card_categorie_id_fkey
+            references card_categories,
+    constraint card_categories_x_card_pkey
+        primary key (card_id, card_categorie_id)
+);
+
+alter table card_categories_x_card
+    owner to postgres;
+
+create table card_item
+(
+    id                 serial not null
+        constraint card_item_pkey
+            primary key,
+    card_categories_id integer
+        constraint card_item_card_categories_id_fkey
+            references card_categories,
+    name               character varying[],
+    price              real,
+    created_at         timestamp,
+    updated_at         timestamp
+);
+
+alter table card_item
+    owner to postgres;
+
+create table promotion_x_card_item
+(
+    promotion_id integer not null
+        constraint promotion_x_card_item_promotion_id_fkey
+            references promotion,
+    card_item_id integer not null
+        constraint promotion_x_card_item_card_item_id_fkey
+            references card_item,
+    constraint promotion_x_card_item_pkey
+        primary key (promotion_id, card_item_id)
+);
+
+alter table promotion_x_card_item
+    owner to postgres;
+
+create table customer_order_line
+(
+    customer_order_id integer not null
+        constraint customer_order_line_customer_order_id_fkey
+            references customer_order,
+    card_item_id      integer not null
+        constraint customer_order_line_card_item_id_fkey
+            references card_item,
+    quantity          real,
+    constraint customer_order_line_pkey
+        primary key (customer_order_id, card_item_id)
+);
+
+alter table customer_order_line
+    owner to postgres;
+
+
