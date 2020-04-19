a1e047bf7279162fc528167f3f95450b2d3dc5c2
Swann HERRERA <swann.herrera@gmail.com>
2020-04-19 11:31:00 +0200

    switch libpng + ajout padding pour décodage

--- qrcode/generate.c
+++ qrcode/generate.c
@@ -3,17 +3,31 @@
 #include <stdbool.h>
 #include <string.h>
 #include "qrcodegen.h"
-#include "libbmp.h"
 #include <png.h>
 #include <curl/curl.h>
 #include <gtk/gtk.h>
 #include <openssl/sha.h>
 
 
+typedef struct {
+    uint8_t red;
+    uint8_t green;
+    uint8_t blue;
+} pixel_t;
+
+typedef struct {
+    pixel_t* pixels;
+    size_t width;
+    size_t height;
+} bitmap_t;
+
 void create_file();
 void sha1_file(FILE* file, char* hash);
-static FILE* saveQr(const uint8_t qrcode[], int size_img_coef, const char* text);
 void on_submit_button_clicked();
+static pixel_t * pixel_at (bitmap_t * bitmap, int x, int y);
+static int save_png_to_file(bitmap_t *bitmap, const char *path);
+static FILE* saveQr(const uint8_t qrcode[], int size_img_coef, const char* text);
+
 
 GtkBuilder* 	builder; 
 
@@ -53,7 +67,7 @@ int main(int argc, char **argv) {
     g_object_unref(builder);
     gtk_main();
 
-		return EXIT_SUCCESS; 
+	return EXIT_SUCCESS; 
 }
 
 void on_submit_button_clicked() {
@@ -87,20 +101,21 @@ void on_submit_button_clicked() {
 	printf("%s\n", hash);
 	
 
-	// Make and print the QR Code symbol
+	// FOR QRCODE
 	uint8_t qrcode_identifier[qrcodegen_BUFFER_LEN_MAX];
 	uint8_t tempBuffer[qrcodegen_BUFFER_LEN_MAX];
-	// Encode argv[1] with qrcodegen_encodeText function
+
+
 	bool isSuccess = qrcodegen_encodeText(hash, tempBuffer, qrcode_identifier, errCorLvl, qrcodegen_VERSION_MIN, qrcodegen_VERSION_MAX, qrcodegen_Mask_AUTO, true);
 	if (isSuccess) {
 		qrcode = saveQr(qrcode_identifier, 20, hash);
 		curl_easy_setopt(curl, CURLOPT_USERPWD, "sftp:GHTinuguErer");
 		curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
 		curl_easy_setopt(curl, CURLOPT_PORT, 2222L);
-		char* remote_url = (char*)malloc(strlen("sftp://51.255.173.90/uploads/") + strlen(hash) + strlen(".bmp"));
+		char* remote_url = (char*)malloc(strlen("sftp://51.255.173.90/uploads/") + strlen(hash) + strlen(".png"));
 		strcpy(remote_url, "sftp://51.255.173.90/uploads/");
 		strcat(remote_url, hash);
-		strcat(remote_url, ".bmp");
+		strcat(remote_url, ".png");
 		curl_easy_setopt(curl, CURLOPT_URL, remote_url);
 		curl_easy_setopt(curl, CURLOPT_READDATA, qrcode);
 
@@ -123,33 +138,67 @@ void on_submit_button_clicked() {
 }
 
 static FILE* saveQr(const uint8_t qrcode[], int size_img_coef, const char* text) {
-	bmp_img img;
+	bitmap_t img;
+	unsigned int padding = 4;
 	FILE* qrcodeFile;
 	int size = qrcodegen_getSize(qrcode);
-	bmp_img_init_df (&img, size * size_img_coef, size * size_img_coef);
-	for (int y = 0; y < size; y++) {
-		for (int yi = 0; yi < size_img_coef; yi += 1) {
-			for (int x = 0; x < size; x++) {
-				for (int xi = 0; xi < size_img_coef; xi += 1) {
+	img.width = (size + padding) * size_img_coef;
+	img.height = (size + padding) * size_img_coef;
+	img.pixels = calloc((img.width * size_img_coef) * (img.height * size_img_coef), sizeof(pixel_t));
+
+	if (!img.pixels) {
+		fprintf(stderr, "Error calloc");
+		exit(EXIT_FAILURE);
+    }
+
+	/**
+	 * This loop make firsts line of pixels white
+	*/
+	for (size_t y = 0; y <= (padding / 2) * size_img_coef; y += 1) {
+		for (size_t x = 0; x < img.width; x += 1) {
+			for (size_t xi = 0; xi < size_img_coef; xi += 1) {
+				pixel_t* pixel = pixel_at(&img, x * size_img_coef + xi, y);
+				pixel->red = 250;
+				pixel->green = 250;
+				pixel->blue = 250;
+			}
+		}
+	}
+	
+	/**
+	 * Main loop write qrcode 
+	 */
+	for (size_t y = 0; y < img.height; y += 1) {
+		for (size_t yi = 0; yi < size_img_coef; yi += 1) {
+			for (size_t x = 0; x < img.width; x += 1) {
+				for (size_t xi = 0; xi < size_img_coef; xi += 1) {
 					if (qrcodegen_getModule(qrcode, x, y)) {
-						bmp_pixel_init(&img.img_pixels[y * size_img_coef + yi][x * size_img_coef + xi], 250, 250, 250);
+						pixel_t* pixel = pixel_at(&img, (x + 2) * size_img_coef + xi, (y + 2) * size_img_coef + yi);
+            			pixel->red = 0;
+            			pixel->green = 0;
+            			pixel->blue = 0;
 					} else {
-						bmp_pixel_init(&img.img_pixels[y * size_img_coef + yi][x * size_img_coef + xi], 0, 0, 0);
+						pixel_t* pixel = pixel_at(&img, (x + 2) * size_img_coef + xi, (y + 2) * size_img_coef + yi);
+						pixel->red = 250;
+						pixel->green = 250;
+						pixel->blue = 250;
 					}
 				}
 			}
 		}
 	}
 
-	// output/ = 7
-	//  .bmp = 4
-	char* filename = malloc(sizeof(char) * (11 + strlen(text)));
+	if (save_png_to_file(&img, "./output/qrcode.png")) {
+		fprintf(stderr, "Error writing file.\n");
+		exit(EXIT_FAILURE);
+    }
+
+    free (img.pixels);
+
+	char* filename = malloc(sizeof(char) * (strlen("output/.png") + strlen(text)));
 	strcpy(filename, "output/");
 	strcat(filename, text);
-	strcat(filename, ".bmp");
-
-	bmp_img_write(&img, filename);
-	bmp_img_free(&img);
+	strcat(filename, ".png");
 
 	qrcodeFile = fopen(filename, "r");
 	free(filename);
@@ -207,4 +256,99 @@ void sha1_file(FILE* file, char* hash) {
 		fprintf(stderr, "Erreur malloc buffer");
 		exit(EXIT_FAILURE);
 	}
+}
+
+
+static int save_png_to_file(bitmap_t *bitmap, const char *path) {
+    FILE * fp;
+    png_structp png_ptr = NULL;
+    png_infop info_ptr = NULL;
+    size_t x, y;
+    png_byte ** row_pointers = NULL;
+    /* "status" contains the return value of this function. At first
+       it is set to a value which means 'failure'. When the routine
+       has finished its work, it is set to a value which means
+       'success'. */
+    int status = -1;
+    /* The following number is set by trial and error only. I cannot
+       see where it it is documented in the libpng manual.
+    */
+    int pixel_size = 3;
+    int depth = 8;
+    
+    fp = fopen (path, "wb");
+    if (! fp) {
+        goto fopen_failed;
+    }
+
+    png_ptr = png_create_write_struct (PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    if (png_ptr == NULL) {
+        goto png_create_write_struct_failed;
+    }
+    
+    info_ptr = png_create_info_struct (png_ptr);
+    if (info_ptr == NULL) {
+        goto png_create_info_struct_failed;
+    }
+    
+    /* Set up error handling. */
+
+    if (setjmp (png_jmpbuf (png_ptr))) {
+        goto png_failure;
+    }
+    
+    /* Set image attributes. */
+
+    png_set_IHDR (png_ptr,
+                  info_ptr,
+                  bitmap->width,
+                  bitmap->height,
+                  depth,
+                  PNG_COLOR_TYPE_RGB,
+                  PNG_INTERLACE_NONE,
+                  PNG_COMPRESSION_TYPE_DEFAULT,
+                  PNG_FILTER_TYPE_DEFAULT);
+    
+    /* Initialize rows of PNG. */
+
+    row_pointers = png_malloc (png_ptr, bitmap->height * sizeof (png_byte *));
+    for (y = 0; y < bitmap->height; y++) {
+        png_byte *row = 
+            png_malloc (png_ptr, sizeof (uint8_t) * bitmap->width * pixel_size);
+        row_pointers[y] = row;
+        for (x = 0; x < bitmap->width; x++) {
+            pixel_t * pixel = pixel_at (bitmap, x, y);
+            *row++ = pixel->red;
+            *row++ = pixel->green;
+            *row++ = pixel->blue;
+        }
+    }
+    
+    /* Write the image data to "fp". */
+
+    png_init_io (png_ptr, fp);
+    png_set_rows (png_ptr, info_ptr, row_pointers);
+    png_write_png (png_ptr, info_ptr, PNG_TRANSFORM_IDENTITY, NULL);
+
+    /* The routine has successfully written the file, so we set
+       "status" to a value which indicates success. */
+
+    status = 0;
+    
+    for (y = 0; y < bitmap->height; y++) {
+        png_free (png_ptr, row_pointers[y]);
+    }
+    png_free (png_ptr, row_pointers);
+    
+ png_failure:
+ png_create_info_struct_failed:
+    png_destroy_write_struct (&png_ptr, &info_ptr);
+ png_create_write_struct_failed:
+    fclose (fp);
+ fopen_failed:
+    return status;
+}
+
+static pixel_t * pixel_at (bitmap_t * bitmap, int x, int y) {
+    return bitmap->pixels + bitmap->width * y + x;
 }
