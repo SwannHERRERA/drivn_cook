88289d58497f059e1fc9eb86138d8cb5e2c24a36
SwannHERRERA <swann.herrera@gmail.com>
2020-07-09 00:51:26 +0200

    upadte setup

--- qrcode/assets/setup.sh
+++ qrcode/assets/setup.sh
@@ -1,3 +1,3 @@
 echo 'Installing dependancies ...'
-sudo apt install libgtk-3-dev glade make libssl-dev libpq-dev libpng-dev libjpeg-dev libcurl4-openssl-dev
+sudo apt install libgtk-3-dev glade make libssl-dev libpq-dev libpng-dev libjpeg-dev libcurl4-openssl-dev appmenu-gtk2-module appmenu-gtk3-module
 # TODO add curl
